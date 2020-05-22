9fa5e4398c7b7b21b8a79bfb83aae135b2cbe9b2
Mark Servilla <mark.servilla@gmail.com>
2020-05-21 22:00:08 -0600

    Add additional query methods

--- src/pinchme/model/resource_db.py
+++ src/pinchme/model/resource_db.py
@@ -16,14 +16,23 @@
 from datetime import datetime
 
 import daiquiri
-from sqlalchemy import Boolean, Column, Integer, String, DateTime, ForeignKey, desc, asc
+from sqlalchemy import (
+    Boolean,
+    Column,
+    Integer,
+    String,
+    DateTime,
+    ForeignKey,
+    desc,
+    asc,
+)
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.sql import not_
 
 from pinchme.config import Config
-
 
 logger = daiquiri.getLogger(__name__)
 
@@ -35,52 +44,65 @@ class Packages(Base):
 
     id = Column(String(), primary_key=True)
     date_created = Column(DateTime(), nullable=False)
+    dirty = Column(Boolean(), nullable=False, default=False)
 
 
 class Resources(Base):
     __tablename__ = "resources"
 
     id = Column(String(), primary_key=True)
+    pid = Column(String(), nullable=False)
+    type = Column(String(), nullable=False)
+    entity_id = Column(String(), nullable=True)
     md5 = Column(String(), nullable=False)
     sha1 = Column(String(), nullable=False)
     checked_count = Column(Integer(), nullable=False, default=0)
     checked_last_date = Column(DateTime(), nullable=True)
     checked_last_status = Column(Boolean(), nullable=True)
+    dirty = Column(Boolean(), nullable=False, default=False)
 
 
 class ResourcePool:
-
     def __init__(self, db: str):
         from sqlalchemy import create_engine
+
         engine = create_engine("sqlite:///" + db)
         Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         self.session = Session()
 
-    def get_package(self, id: str) -> Packages:
+    def get_clean_packages(self) -> list:
         p = None
         try:
-            p = self.session.query(Packages).filter(
-                    Packages.id == id).one()
+            p = self.session.query(Packages).filter(not_(Packages.dirty)).all()
         except NoResultFound as e:
             logger.error(e)
         return p
 
     def get_last_package_create_date(self) -> datetime:
-        p = None
+        d = None
         try:
-            p = self.session.query(Packages).order_by(
-                Packages.date_created.desc()
-            ).first()
+            p = (
+                self.session.query(Packages)
+                .order_by(Packages.date_created.desc())
+                .first()
+            )
+            if p is not None:
+                d = p.date_created
         except NoResultFound as e:
             logger.error(e)
-        return p.date_created
+        return d
+
+    def get_package(self, id: str) -> Packages:
+        p = None
+        try:
+            p = self.session.query(Packages).filter(Packages.id == id).one()
+        except NoResultFound as e:
+            logger.error(e)
+        return p
 
     def insert_package(self, id: str, date_created: datetime):
-        p = Packages(
-            id=id,
-            date_created=date_created
-        )
+        p = Packages(id=id, date_created=date_created)
         try:
             self.session.add(p)
             self.session.commit()
@@ -89,11 +111,63 @@ class ResourcePool:
             self.session.rollback()
             raise e
 
-    def insert_resource(self, id: str, md5: str, sha1: str):
+    def set_clean_packages(self):
+        try:
+            p = self.session.query(Packages).update({Packages.dirty: False})
+            self.session.commit()
+        except NoResultFound as e:
+            logger.error(e)
+            raise e
+
+    def set_dirty_package(self, id: str):
+        try:
+            p = (
+                self.session.query(Packages)
+                .filter(Packages.id == id)
+                .update({Packages.dirty: True})
+            )
+            self.session.commit()
+        except NoResultFound as e:
+            logger.error(e)
+            raise e
+
+    def get_clean_resources(self) -> list:
+        r = None
+        try:
+            r = (
+                self.session.query(Resources)
+                .filter(not_(Resources.dirty))
+                .all()
+            )
+        except NoResultFound as e:
+            logger.error(e)
+        return r
+
+    def get_package_resources(self, pid: str) -> list:
+        r = None
+        try:
+            r = (
+                self.session.query(Resources)
+                .filter(Resources.pid == pid)
+                .all()
+            )
+        except NoResultFound as e:
+            logger.error(e)
+        return r
+
+    def get_resource(self, id: str) -> Resources:
+        p = None
+        try:
+            p = self.session.query(Resources).filter(Resources.id == id).one()
+        except NoResultFound as e:
+            logger.error(e)
+        return p
+
+    def insert_resource(
+        self, id: str, pid: str, type: str, entity_id: str, md5: str, sha1: str
+    ):
         r = Resources(
-            id=id,
-            md5=md5,
-            sha1=sha1,
+            id=id, pid=pid, type=type, entity_id=entity_id, md5=md5, sha1=sha1,
         )
         try:
             self.session.add(r)
@@ -103,12 +177,22 @@ class ResourcePool:
             self.session.rollback()
             raise e
 
-    def get_resource(self, id: str) -> Resources:
-        p = None
+    def set_clean_resources(self):
         try:
-            p = self.session.query(Resources).filter(
-                    Resources.id == id).one()
+            r = self.session.query(Resources).update({Resources.dirty: False})
+            self.session.commit()
         except NoResultFound as e:
             logger.error(e)
-        return p
+            raise e
 
+    def set_dirty_resource(self, id: str):
+        try:
+            r = (
+                self.session.query(Resources)
+                .filter(Resources.id == id)
+                .update({Resources.dirty: True})
+            )
+            self.session.commit()
+        except NoResultFound as e:
+            logger.error(e)
+            raise e
--- tests/test_model.py
+++ tests/test_model.py
@@ -23,9 +23,62 @@ from pinchme.model.resource_db import ResourcePool, Packages, Resources
 
 
 PACKAGE_ID = "edi.1.1"
+
 RESOURCE_ID = "https://pasta.lternet.edu/package/data/eml/edi/1/1/482fef41e108b34ad816e96423711470"
+RESOURCE_TYPE = "data"
+RESOURCE_NAME = "482fef41e108b34ad816e96423711470"
 RESOURCE_MD5 = "9cbb385955353ef614b8f300602c4b8c"
 RESOURCE_SHA1 = "f494400be17e301e9388bd542cfb9b2c91caaef3"
+
+TEST_PACKAGE_DATA = [
+    ("edi.1.1", datetime(2016, 12, 1, 12, 55, 8, 778000)),
+    ("edi.2.1", datetime(2016, 12, 1, 13, 15, 38, 166000)),
+    ("edi.3.1", datetime(2016, 12, 21, 15, 53, 42, 211000)),
+    ("edi.4.1", datetime(2017, 1, 2, 10, 29, 4, 33000)),
+    ("edi.7.1", datetime(2017, 1, 7, 10, 39, 36, 319000)),
+    ("edi.5.1", datetime(2017, 1, 7, 11, 20, 53, 526000)),
+    ("edi.6.1", datetime(2017, 1, 7, 12, 12, 1, 678000)),
+    ("edi.4.2", datetime(2017, 2, 13, 12, 56, 59, 337000)),
+    ("edi.8.1", datetime(2017, 3, 5, 17, 43, 4, 215000)),
+    ("edi.8.2", datetime(2017, 3, 10, 14, 13, 1, 911000)),
+]
+
+TEST_RESOURCE_DATA = [
+    (
+        "https://pasta.lternet.edu/package/metadata/eml/edi/1/1",
+        "edi.1.1",
+        "metadata",
+        None,
+        "412e30ed380fafdf8654ae7495d1f3f6",
+        "e698c128e4fdbc18767e467026349afbb30423db",
+    ),
+    (
+        "https://pasta.lternet.edu/package/report/eml/edi/1/1",
+        "edi.1.1",
+        "report",
+        None,
+        "711efe73e09a02832d693c23abefb94f",
+        "5c4e60e12d814148b7647f348c362ab4b4678245",
+    ),
+    (
+        "https://pasta.lternet.edu/package/data/eml/edi/1/1"
+        "/482fef41e108b34ad816e96423711470",
+        "edi.1.1",
+        "data",
+        "482fef41e108b34ad816e96423711470",
+        "9cbb385955353ef614b8f300602c4b8c",
+        "f494400be17e301e9388bd542cfb9b2c91caaef3",
+    ),
+    (
+        "https://pasta.lternet.edu/package/data/eml/edi/1/1"
+        "/cba4645e845957d015008e7bccf4f902",
+        "edi.1.1",
+        "data",
+        "cba4645e845957d015008e7bccf4f902",
+        "85a4466056f5fce9255ca28fe6d1827a",
+        "c1d6e1099634dd36f4e40b7da2558b12dc45b149",
+    ),
+]
 
 
 @pytest.fixture()
@@ -56,33 +109,96 @@ def test_insert_get_package(rp, clean_up):
 
 
 def test_insert_get_resource(rp, clean_up):
-    rp.insert_resource(RESOURCE_ID, RESOURCE_MD5, RESOURCE_SHA1)
+    rp.insert_resource(
+        RESOURCE_ID, PACKAGE_ID, RESOURCE_TYPE, RESOURCE_NAME, RESOURCE_MD5, RESOURCE_SHA1
+    )
     r = rp.get_resource(RESOURCE_ID)
     assert isinstance(r, Resources)
     assert r.id == RESOURCE_ID
+    assert r.pid == PACKAGE_ID
+    assert r.type == RESOURCE_TYPE
+    assert r.entity_id == RESOURCE_NAME
     assert r.md5 == RESOURCE_MD5
     assert r.sha1 == RESOURCE_SHA1
 
 
 def test_get_last_package_create_date(rp, clean_up):
-    test_data = [
-        ("edi.1.1", datetime(2016, 12, 1, 12, 55, 8, 778000)),
-        ("edi.2.1", datetime(2016, 12, 1, 13, 15, 38, 166000)),
-        ("edi.3.1", datetime(2016, 12, 21, 15, 53, 42, 211000)),
-        ("edi.4.1", datetime(2017, 1, 2, 10, 29, 4, 33000)),
-        ("edi.7.1", datetime(2017, 1, 7, 10, 39, 36, 319000)),
-        ("edi.5.1", datetime(2017, 1, 7, 11, 20, 53, 526000)),
-        ("edi.6.1", datetime(2017, 1, 7, 12, 12, 1, 678000)),
-        ("edi.4.2", datetime(2017, 2, 13, 12, 56, 59, 337000)),
-        ("edi.8.1", datetime(2017, 3, 5, 17, 43, 4, 215000)),
-        ("edi.8.2", datetime(2017, 3, 10, 14, 13, 1, 911000)),
-    ]
-
-    for package in test_data:
+    for package in TEST_PACKAGE_DATA:
         rp.insert_package(package[0], package[1])
-
     d = rp.get_last_package_create_date()
     assert d is not None
     assert isinstance(d, datetime)
     assert d == datetime(2017, 3, 10, 14, 13, 1, 911000)
+
+
+def test_get_last_package_create_date_from_empty_db(rp, clean_up):
+    d = rp.get_last_package_create_date()
+    assert d is None
+
+
+def test_set_dirty_package(rp, clean_up):
+    rp.insert_package(PACKAGE_ID, pendulum.now())
+    p = rp.get_package(PACKAGE_ID)
+    assert not p.dirty
+    rp.set_dirty_package(PACKAGE_ID)
+    p = rp.get_package(PACKAGE_ID)
+    assert p.dirty
+
+
+def test_get_clean_packages(rp, clean_up):
+    pre = len(TEST_PACKAGE_DATA)
+    for package in TEST_PACKAGE_DATA:
+        rp.insert_package(package[0], package[1])
+    rp.set_dirty_package(PACKAGE_ID)
+    p = rp.get_clean_packages()
+    post = len(p)
+    assert pre == post + 1
+
+
+def test_set_dirty_resource(rp, clean_up):
+    rp.insert_resource(
+        RESOURCE_ID, PACKAGE_ID, RESOURCE_TYPE, RESOURCE_NAME, RESOURCE_MD5, RESOURCE_SHA1
+    )
+    r = rp.get_resource(RESOURCE_ID)
+    assert not r.dirty
+    rp.set_dirty_resource(RESOURCE_ID)
+    r = rp.get_resource(RESOURCE_ID)
+    assert r.dirty
+
+
+def test_get_clean_resources(rp, clean_up):
+    pre = len(TEST_RESOURCE_DATA)
+    for resource in TEST_RESOURCE_DATA:
+        rp.insert_resource(
+            resource[0], resource[1], resource[2], resource[3], resource[4], resource[5]
+        )
+    rp.set_dirty_resource(RESOURCE_ID)
+    r = rp.get_clean_resources()
+    post = len(r)
+    assert pre == post + 1
+
+
+def test_clean_all_resources(rp, clean_up):
+    for resource in TEST_RESOURCE_DATA:
+        rp.insert_resource(
+            resource[0], resource[1], resource[2], resource[3], resource[4], resource[5]
+        )
+        rp.set_dirty_resource(resource[0])
+    for resource in TEST_RESOURCE_DATA:
+        r = rp.get_resource(resource[0])
+        assert r.dirty
+    rp.set_clean_resources()
+    for resource in TEST_RESOURCE_DATA:
+        r = rp.get_resource(resource[0])
+        assert not r.dirty
+
+
+def test_get_package_resources(rp, clean_up):
+    for resource in TEST_RESOURCE_DATA:
+        rp.insert_resource(
+            resource[0], resource[1], resource[2], resource[3], resource[4], resource[5]
+        )
+    resources = rp.get_package_resources(PACKAGE_ID)
+    for resource in resources:
+        assert PACKAGE_ID in resource.pid
 
