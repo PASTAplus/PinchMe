71a9acb4d421f57ef0e60fb36be2d982f4afb8cc
Mark Servilla <mark.servilla@gmail.com>
2024-11-06 01:03:02 +0000

    Use pathlib.Path in lieu of os.path; reformt packages and python_requires

--- setup.py
+++ setup.py
@@ -11,38 +11,38 @@
 :Created:
     5/18/2020
 """
-from os import path
+from pathlib import Path
 from setuptools import find_packages, setup
 
 
-here = path.abspath(path.dirname(__file__))
+here = Path(".").resolve()
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(here / 'README.md', encoding='utf-8') as f:
     long_description = f.read()
 
-with open(path.join(here, 'LICENSE'), encoding='utf-8') as f:
+with open(here / 'LICENSE', encoding='utf-8') as f:
     full_license = f.read()
 
 setup(
     name="pinchme",
     version="2024.11.03",
-    description="Perform PASTA+ resource integrity analysis",
+    description="Perform PASTA resource integrity analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author="PASTA+ project",
+    author="Mark Servilla",
     url="https://github.com/PASTAplus/PinchMe",
     license=full_license,
-    packages=find_packages(where="src"),
+    packages=find_packages(where="src", include=["pinchme"]),
     include_package_data=True,
     exclude_package_data={"": ["settings.py, properties.py, config.py"],},
     package_dir={"": "src"},
-    python_requires=">3.11",
+    python_requires=" >= 3.11",
     install_requires=[
         "click >= 8.1.7",
         "daiquiri >= 3.0.0",
         "sqlalchemy >= 1.4.51",
         "pendulum >= 2.1.2"
-        ],
+    ],
     entry_points={"console_scripts": ["pinchme=pinchme.pinchy:main"]},
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
