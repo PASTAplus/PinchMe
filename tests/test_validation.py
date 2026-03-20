41a822245c403542c71702e762ab8cbc445901e5
Mark Servilla <mark.servilla@gmail.com>
2026-03-19 18:58:39 -0600

    Summary: Add RESOURCE_SIZE and RESOURCE_LOCATION to test resource.

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -27,6 +27,8 @@ RESOURCE_TYPE = "data"
 RESOURCE_NAME = "482fef41e108b34ad816e96423711470"
 RESOURCE_MD5 = "9cbb385955353ef614b8f300602c4b8c"
 RESOURCE_SHA1 = "f494400be17e301e9388bd542cfb9b2c91caaef3"
+RESOURCE_SIZE = 1516906
+RESOURCE_LOCATION = str(Path.cwd().resolve() / "data/edi.1.1/482fef41e108b34ad816e96423711470")
 
 
 @pytest.fixture()
@@ -49,6 +51,8 @@ def test_valid_md5(rp, clean_up):
         RESOURCE_NAME,
         RESOURCE_MD5,
         RESOURCE_SHA1,
+        RESOURCE_SIZE,
+        RESOURCE_LOCATION
     )
     r = rp.get_resource(RESOURCE_ID)
     assert validation.valid_md5(r)
